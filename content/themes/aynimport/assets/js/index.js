77ff27d43aa5d78e55751e06c8fa97715914b92b
Guille Cura <cura.gf@gmail.com>
2016-06-30 23:56:51 -0300

    Home page desktop version.

--- content/themes/aynimport/assets/js/index.js
+++ content/themes/aynimport/assets/js/index.js
@@ -14,15 +14,41 @@
     var $postContent = $(".post-content");
     $postContent.fitVids();
 
-    // $document.on('scroll', function () {
-    //   $('.hero-section').css(
-    //     'background-position-y', $document.scrollTop() / 2);
-    // });
+    // Smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+    // Sticky navs
+    var $mainNav = $("header.main-nav");
+    var height = $(window).height() - 100;
+
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      // Home page
+      if (scroll >= height) {
+        $mainNav.addClass("active");
+      } else {
+        $mainNav.removeClass("active");
+      }
+    });
+
 
     // Menu & menu button animation
     var $btn = $(".menu-icon");
+    var $btnNav = $("body > nav > ul a");
     var $body = $("body");
-    var $nav = $("body > nav > ul");
+    var $nav = $("body > nav");
 
     $btn.on("click tap", function(e){
       e.preventDefault();
@@ -38,7 +64,6 @@
     });
 
     // Gallery filters
-
     var $tag = $(".filters span");
     var $imageItem = $(".content .gallery-image");
 
